caf0a855c1bb6358e1663a92dff526e30a131360
shofas0921 <motohashi.sho@gmail.com>
2024-04-10 22:37:26 +0900

    2024/04/10 Skills component Github API test connect

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 export const Skills = () => {
+  // const outputTest = () => { console.log('TEST'); };
+
+  // useEffect(outputTest);
+
+  useEffect(() => { axios.get('https://api.github.com/users/shofas0921/repos').then((response)=>console.log(response)) }, []);
+
   return(
     <div id="skills">
       <div className="container">
